7973a26b120e463346f614225b48c865fcd592ca
nlewis84 <nathan@differential.com>
2021-12-20 21:07:05 -0600

    feat: working routes for all and show pages

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
 import React from 'react';
-
+import { Router, Link } from '@reach/router';
 import './App.css';
 
-class App extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
+import All from './pages/All/index';
+import Show from './pages/Show/index';
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  // ComponentDidMount is used to
-  // execute the code
-  componentDidMount() {
-    fetch(`${process.env.REACT_APP_DATA_URL}`)
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Loading...</h1>{' '}
-        </div>
-      );
-
-    return (
-      <div className="App">
-        <h1> Fetch data from an api in react </h1>{' '}
-        {items.map((item) => (
-          <ol key={item.id}>
-            Title: {item.title}, Composer: {item.composer}
-          </ol>
-        ))}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Percussion Library</h1>
+      <Router>
+        <All path="/" />
+        <Show path="ensembles/:ensembleId" />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
