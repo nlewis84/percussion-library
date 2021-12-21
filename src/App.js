2b497e5d7721a56897447fea1fe470bef62acc3c
nlewis84 <nathan@differential.com>
2021-12-20 20:09:35 -0600

    chore: updated ignored files and working All page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_DATA_URL}`)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Loading...</h1>{' '}
+        </div>
+      );
+
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>{' '}
+        {items.map((item) => (
+          <ol key={item.id}>
+            Title: {item.title}, Composer: {item.composer}
+          </ol>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
