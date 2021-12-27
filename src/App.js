a8b8cd1fcd5404871b6059bb2e9b1992531dfb35
nlewis84 <nathan@differential.com>
2021-12-26 21:59:03 -0600

    chore: added eslint and setup first pass with MUI' '

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import './App.css';
+import { Container, Link, Typography } from '@mui/material';
 
 import All from './pages/All/index';
 import Show from './pages/Show/index';
 
+function Copyright() {
+	return (
+		<Typography variant="body2" color="text.secondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://mui.com/">
+        Percussion Library
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
+
 function App() {
-  return (
-    <div className="wrapper">
-      <h1>Percussion Library</h1>
-      <Router>
-        <All path="/" />
-        <Show path="ensembles/:ensembleId" />
-      </Router>
-    </div>
-  );
+	return (
+		<Container className="wrapper">
+			<Typography variant="h1" component="h1">
+        Percussion Library
+			</Typography>
+			<Router>
+				<All path="/" />
+				<Show path="ensembles/:ensembleId" />
+			</Router>
+			<Copyright />
+		</Container>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import theme from './theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
