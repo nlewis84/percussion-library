a133acbd488690a26f9c3d4b7b968d016f7348b4
nlewis84 <nathan@differential.com>
2021-12-26 23:02:30 -0600

    chroe:: more mui setup

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import App from './App';
 import theme from './theme';
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<App />
+	</ThemeProvider>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/All/index.js
+++ src/pages/All/index.js
@@ -1,47 +1,68 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { Card, CardContent, Container, Paper, Typography } from '@mui/material';
+import SanitizeDifficulty from '../../helpers/sanitizeDifficuty';
 
 class All extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
+	// Constructor
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
+		this.state = {
+			items: [],
+			DataisLoaded: false,
+		};
+	}
 
-  componentDidMount() {
-    fetch(`${process.env.REACT_APP_DATA_URL}`)
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Loading...</h1>{' '}
-        </div>
-      );
+	componentDidMount() {
+		fetch(`${process.env.REACT_APP_DATA_URL}`)
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true,
+				});
+			});
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded)
+			return (
+				<Container>
+					<Card sx={{ minWidth: 275 }}>
+						<CardContent>
+							<Typography variant='h1'> Loading...</Typography>{' '}
+						</CardContent>
+					</Card>
+				</Container>
+			);
 
-    return (
-      <div className="App">
-        {items.map((item) => (
-          <ol key={item.id}>
-            <Link to={`ensembles/${item.id}`}>{item.title}</Link> by{' '}
-            {item.composer}
-          </ol>
-        ))}
-      </div>
-    );
-  }
+		return (
+			<Container className="App">
+				{items.map((item) => (
+					<Paper key={item.id} >
+						<Card sx={{ maxWidth: 275 }} variant="outlined">
+							<CardContent>
+								<Typography variant="h5" component="div">
+									{item.title}
+								</Typography>
+								<Typography sx={{ mb: 1.5 }} color="text.secondary">
+									{item.composer}
+								</Typography>
+								<Typography variant="body2">
+									{SanitizeDifficulty(item.level)}
+									<br />
+									{item.players}
+								</Typography>
+								<Link to={`ensembles/${item.id}`}>{item.title}</Link> by{' '}
+								{item.composer}
+							</CardContent>
+						</Card>
+					</Paper>
+				))}
+			</Container>
+		);
+	}
 }
 
 export default All;
