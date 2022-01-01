41c1c80e7873440623195ac8c543c308a3584707
nlewis84 <nathan@differential.com>
2022-01-01 11:11:59 -0600

    feat: updated layout on Show page and added a description format helper

---
+++ src/helpers/descriptionFormatter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function DescriptionFormatter(title, description) {
+	let regex = title;
+	let myArray = description.split(regex);
+  
+	return (myArray.map((item, i) => 
+	// Check for first and last items being the title of the piece, and styles appropriately.
+		item === '' 
+			? <Typography key={i} component={'span'} variant="body1" sx={{ display: 'inline' }} color="primary.main">{title}</Typography> 
+		// Check for last item of array and don't add a title to the end.
+			: item === myArray[myArray.length - 1]
+				? <Typography key={i} variant="body2" sx={{ display: 'inline' }} color="text.primary">{item}</Typography>
+				: <Typography key={i} variant="body2" sx={{ display: 'inline' }} color="text.primary">{item}
+					<Typography key={i} component={'span'} variant="body1" sx={{ display: 'inline' }} color="primary.main">{title}</Typography>
+				</Typography>
+	));
+}
+
+export default DescriptionFormatter;
--- src/pages/Show/index.js
+++ src/pages/Show/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardContent, Container, Link, Typography } from '@mui/material';
+
+import SanitizeDifficulty from '../../helpers/sanitizeDifficuty';
+import DescriptionFormatter from '../../helpers/descriptionFormatter';
 
 class Show extends React.Component {
 	// Constructor
@@ -39,31 +43,36 @@ class Show extends React.Component {
 		return (
 			<Container className="App">
 				<Typography variant="body2" sx={{ mb: 1.5 }} color="text.secondary">{item.category}</Typography>
-				<Typography variant="h3" sx={{ mb: 1.5 }} color="primary.main" >{item.title}</Typography>
-				{item.compilation ? <Typography variant="h6" color="text.secondary">{item.compilation}</Typography> : null}
-				{item.arranger ? <Typography variant="h6" color="text.secondary">{item.arranger}</Typography> : <Typography variant="h6" color="text.secondary">{item.composer}</Typography>}
-				{item.level ? <Typography variant="body" color="text.secondary"><strong>Level: </strong>{item.level}</Typography> : null}
-				{item.duration ? <Typography variant="body1" color="text.secondary"><strong>Duration: </strong>{item.duration}</Typography> : null}
-				{item.players ? <Typography variant="body1" color="text.secondary"><strong>Personnel: </strong>{item.players}</Typography> : null}
+				<Typography variant="h3" sx={{ fontWeight: 700, mb: 1.5 }} color="primary.main" >{item.title}</Typography>
+				{item.compilation ? <Typography variant="h6" color="text.primary">{item.compilation}</Typography> : null}
+				{item.arranger ? <Typography variant="h6" color="text.primary">{item.arranger}</Typography> : <Typography variant="h6" color="text.primary">{item.composer}</Typography>}
+				{item.level ? <Typography variant="body2" color="text.secondary">Level</Typography> : null}
+				{item.level ? <Typography variant="body1" color="text.primary">{SanitizeDifficulty(item.level)}</Typography> : null}
+				{item.duration ? <Typography variant="body2" color="text.secondary">Duration</Typography> : null}
+				{item.duration ? <Typography variant="body1" color="text.primary">{item.duration}</Typography> : null}
+				{item.players ? <Typography variant="body2" color="text.secondary">Personnel</Typography> : null}
+				{item.players ? <Typography variant="body1" color="text.primary">{item.players}</Typography> : null}
+				{item.instrumentation ? <Typography variant="body2" color="text.secondary">Instrumentation</Typography> : null}
 				{/* split this on the bullet */}
 				{item.instrumentation ? (
-					<Typography variant="body1" color="text.secondary">{item.instrumentation}</Typography>
+					<Typography variant="body1" sx={{ mb: 1.5 }} color="text.primary">{item.instrumentation}</Typography>
 				) : null}
-				<br />
-				{item.description ? <Typography variant="body1" color="text.secondary">{item.description}</Typography> : null}
-				<br />
+				{item.description ? DescriptionFormatter(item.title, item.description) : null}
 				{/* embed Sound Cloud player here */}
 				{item.audio_link ? (
-					<Typography variant="body2" color="text.secondary">
+					<Typography variant="body2" sx={{ mb: 1.5 }} color="text.primary">
             Audio Link: <Link href={item.audio_link}>Sound Cloud</Link>
 					</Typography>
 				) : null}
-				<br />
 				{/* make these reviews look cool */}
-				{item.reviews ? <Typography variant="body3" color="text.secondary">{item.reviews}</Typography> : null}
+				{item.reviews ? <Typography variant="body3" color="text.primary">{item.reviews}</Typography> : null}
 			</Container>
 		);
 	}
 }
+
+Show.propTypes = {
+	ensembleId: PropTypes.string
+};
 
 export default Show;
