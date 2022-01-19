9f822b76a538deb69f6cb2b35a4164a06553b858
nlewis84 <nathan@differential.com>
2022-01-19 16:41:53 -0600

    chore: began work on hooking up player filter (not working yet)

--- src/pages/Ensembles/All/AllEnsembles.js
+++ src/pages/Ensembles/All/AllEnsembles.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { Box, Card, CardContent, Container, Grid, Paper, Skeleton, Stack, Typography } from '@mui/material';
+import {
+	Box,
+	Card,
+	CardContent,
+	Checkbox,
+	Container,
+	FormControl,
+	Grid,
+	InputLabel,
+	ListItemText,
+	MenuItem,
+	OutlinedInput,
+	Paper,
+	Select,
+	Skeleton,
+	Stack,
+	Typography
+} from '@mui/material';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import SpeedOutlinedIcon from '@mui/icons-material/SpeedOutlined';
 import SanitizeDifficulty from '../../../helpers/sanitizeDifficuty';
 import TruncateText from '../../../helpers/truncateText';
-import SelectFilter from '../../../components/SelectFilter';
 
 class AllEnsembles extends React.Component {
 	// Constructor
@@ -15,6 +31,7 @@ class AllEnsembles extends React.Component {
 		this.state = {
 			items: [],
 			DataisLoaded: false,
+			numberOfPlayers: [],
 		};
 	}
 
@@ -30,7 +47,34 @@ class AllEnsembles extends React.Component {
 			});
 	}
 	render() {
-		const { DataisLoaded, items } = this.state;
+		const { DataisLoaded, items, numberOfPlayers } = this.state;
+
+		const ITEM_HEIGHT = 48;
+		const ITEM_PADDING_TOP = 8;
+		const MenuProps = {
+			PaperProps: {
+				style: {
+					maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+					width: 250,
+				},
+			},
+		};
+
+		const number = [
+			'2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13+'
+		];
+		
+		const handleChange = (event) => {
+			const {
+				target: { value },
+			} = event;
+			let filteredItems = items.filter(item => value.includes(item.min_players));
+			this.setState({
+				items: filteredItems,
+				numberOfPlayers: typeof value === 'string' ? value.split(',') : value,
+			});
+		};
+
 		if (!DataisLoaded)
 			return (
 				<Container>
@@ -65,7 +109,29 @@ class AllEnsembles extends React.Component {
 		return (
 			<Container className="App">
 				<Paper  sx={{ mt: 2, display: 'block' }} alignItems="center" justifyContent="center">
-					<SelectFilter />
+					
+					<FormControl sx={{ m: 2, width: 300 }}>
+						<InputLabel id="multiple-checkbox-label" sx={{bgcolor: 'background.paper', pr: 1}}>Number of Players</InputLabel>
+						<Select
+							labelId="multiple-checkbox-label"
+							id="multiple-checkbox"
+							multiple
+							value={numberOfPlayers}
+							onChange={handleChange}
+							input={<OutlinedInput label="Tag" />}
+							renderValue={(selected) => selected.join(', ')}
+							MenuProps={MenuProps}
+							labelWidth={300}
+						>
+							{number.map((num) => (
+								<MenuItem key={num} value={num}>
+									<Checkbox checked={numberOfPlayers.indexOf(num) > -1} />
+									<ListItemText primary={num} />
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+
 					<Typography variant="body" color="secondary.main" textAlign="center" sx={{ display: 'block'}}>{items.filter((item) => item.category === 'Percussion Ensembles').length} results</Typography>
 				</Paper>
 				<Grid container sx={{ gap: 2, mt: 5 }} spacing={2} alignItems="center" justifyContent="center">
